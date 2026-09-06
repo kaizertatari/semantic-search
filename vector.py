2050b513b4511af145b61b2591564a35598f0e13
johndoe9787 <9787johndoe@gmail.com>
2026-09-05 20:49:51 -0500

    Updated vector program

--- vector.py
+++ vector.py
@@ -1,12 +1,17 @@
 
 from sentence_transformers import SentenceTransformer
+import numpy as np
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
 vectors = model.encode(["car", "carpet", "automobile"])
 
 
-def output():
-    print (len(vectors[0]))
+def similarity(a,b):
+    answer = np.dot(a,b)
+    return answer
 
+print(f"Car and Carpet: {similarity(vectors[0],vectors[1])}")
 
-output()
+print(f"Car and automobile: {similarity(vectors[0],vectors[2])}")
+
+print(f"Carpet and automobile: {similarity(vectors[1],vectors[2])}")
