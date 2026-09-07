854b0826e5060dd3f25d8475e4e542c199af7d68
johndoe9787 <9787johndoe@gmail.com>
2026-09-07 13:54:07 -0500

    Fix similarity() to compute true cosine not bare dot product

--- vector.py
+++ vector.py
@@ -7,7 +7,7 @@ vectors = model.encode(["car", "carpet", "automobile"])
 
 
 def similarity(a,b):
-    answer = np.dot(a,b)
+    answer =np.dot(a,b) / (np.linalg.norm(a) * np.linalg.norm(b))
     return answer
 
 print(f"Car and Carpet: {similarity(vectors[0],vectors[1])}")
@@ -15,3 +15,5 @@ print(f"Car and Carpet: {similarity(vectors[0],vectors[1])}")
 print(f"Car and automobile: {similarity(vectors[0],vectors[2])}")
 
 print(f"Carpet and automobile: {similarity(vectors[1],vectors[2])}")
+
+print(np.linalg.norm(vectors[1]))
