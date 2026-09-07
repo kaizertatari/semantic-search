a34351133e978f781195071f7572739995db451c
Kaizertatari <294377755+kaizertatari@users.noreply.github.com>
2026-09-07 18:47:29 -0500

    Split documents into paragraph chunks

--- search.py
+++ search.py
@@ -10,9 +10,11 @@ file_data = []
 for p in Path("docs").glob("*.txt"):
     content = p.read_text(encoding = "utf-8")
     file = p.name
-    charcount = len(content)
 
-    file_data.append({"text":content,"filename":file,"length":charcount})
+    for para in content.split("\n\n"):
+        file_data.append({"text":para,"filename":file})
 
 print(file_data[1])
+print(len(file_data))
+print(len(file_data[1]))
 
