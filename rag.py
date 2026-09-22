e0133230b54b97f4c7298e2b09c66a04a935d98a
Kaizertatari <294377755+kaizertatari@users.noreply.github.com>
2026-09-22 13:12:48 -0500

    Print prompt with top 3 chunks sorted in descending order of score

--- rag.py
+++ rag.py
@@ -7,17 +7,24 @@ def get_input():
 
 load_dotenv()
 client = Groq()
-question = f"Which of these help you find bugs.\n\n".join({file_data["text"]},{file_data["filename"]})
-print(question)
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": question
-        }
-    ],
-    model="groq/compound-mini"
-)
+datalist = []
+for data in file_data[:3]:
+    datalist.append(f"Text: {data["text"]} Filename: {data["filename"]}")
+  
+question = "Which of these help you find bugs.\n"
+prompt = question  + "\n\n".join(datalist)
+
+print(prompt)
+
+# #chat_completion = client.chat.completions.create(
+#     messages=[
+#         {
+#             "role": "user",
+#             "content": question
+#         }
+#     ],
+#     model="groq/compound-mini"
+# )*/
 
 #print(chat_completion.choices[0].message.content)
 #print([m.id for m in client.models.list().data])*/
