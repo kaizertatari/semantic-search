450ecfabed84de6e1424b076b3a78c323c3cbf74
Kaizertatari <294377755+kaizertatari@users.noreply.github.com>
2026-09-23 12:18:10 -0500

    Updated Groq model to openai/gpt-oss-20b and prompt

--- rag.py
+++ rag.py
@@ -1,30 +1,27 @@
 from groq import Groq
 from dotenv import load_dotenv
 import os
-from search import file_data
+from search import search
 def get_input():
     return input("Enter the prompt: ")
 
 load_dotenv()
 client = Groq()
-datalist = []
-for data in file_data[:3]:
-    datalist.append(f"Text: {data["text"]} Filename: {data["filename"]}")
-  
-question = "Which of these help you find bugs.\n"
+
+question = "What's a good way to cook rice?\n"
+datalist = search(question)
 prompt = question  + "\n\n".join(datalist)
 
 print(prompt)
 
-# #chat_completion = client.chat.completions.create(
-#     messages=[
-#         {
-#             "role": "user",
-#             "content": question
-#         }
-#     ],
-#     model="groq/compound-mini"
-# )*/
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": prompt
+        }
+    ],
+    model="openai/gpt-oss-20b"
+)
 
-#print(chat_completion.choices[0].message.content)
-#print([m.id for m in client.models.list().data])*/
+print(chat_completion.choices[0].message.content)
