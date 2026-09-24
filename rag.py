ad20c7f418efa493cfadf4a0f2e313ca0c33ef05
Kaizertatari <294377755+kaizertatari@users.noreply.github.com>
2026-09-24 14:38:34 -0500

    API key and client are created once at module level

--- rag.py
+++ rag.py
@@ -2,16 +2,17 @@ from groq import Groq
 from dotenv import load_dotenv
 import os
 from search import search
+
+load_dotenv()
+client = Groq()
+
 def get_answer(question):
-    load_dotenv()
-    client = Groq()
+    
 
     datalist = search(question)
     instruction = "\n\nOnly use the information above i gave you to answer the question. Don't invent any information if there is none and ignore any information that doesn't help answer the question. Name the filename for each point you make."
 
     prompt = question  + "\n" + "\n\n".join(datalist) + instruction
-
-    print(prompt)
 
     chat_completion = client.chat.completions.create(
         messages=[
@@ -23,4 +24,4 @@ def get_answer(question):
         model="openai/gpt-oss-20b"
     )
 
-    return chat_completion.choices[0].message.content
+    return  (prompt + "\n\n" + chat_completion.choices[0].message.content)
