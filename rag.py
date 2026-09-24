e5e07fd481afefb7eb415b5c9030a4abfa1f20ac
Kaizertatari <294377755+kaizertatari@users.noreply.github.com>
2026-09-24 14:32:55 -0500

    Refracted code for rag.py to function that gets a question from app.py and returns the answer.

--- rag.py
+++ rag.py
@@ -2,32 +2,25 @@ from groq import Groq
 from dotenv import load_dotenv
 import os
 from search import search
-def get_input():
-    answer = input("Enter the prompt: ")
-    while not answer.strip():
-        print("Prompt must not be blank.")
-        answer = input("Enter the prompt: ")
-    return answer
+def get_answer(question):
+    load_dotenv()
+    client = Groq()
 
-load_dotenv()
-client = Groq()
+    datalist = search(question)
+    instruction = "\n\nOnly use the information above i gave you to answer the question. Don't invent any information if there is none and ignore any information that doesn't help answer the question. Name the filename for each point you make."
 
-question = get_input()
-datalist = search(question)
-instruction = "\n\nOnly use the information above i gave you to answer the question. Don't invent any information if there is none and ignore any information that doesn't help answer the question. Name the filename for each point you make."
+    prompt = question  + "\n" + "\n\n".join(datalist) + instruction
 
-prompt = question  + "\n" + "\n\n".join(datalist) + instruction
+    print(prompt)
 
-print(prompt)
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+        model="openai/gpt-oss-20b"
+    )
 
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": prompt
-        }
-    ],
-    model="openai/gpt-oss-20b"
-)
-
-print(chat_completion.choices[0].message.content)
+    return chat_completion.choices[0].message.content
